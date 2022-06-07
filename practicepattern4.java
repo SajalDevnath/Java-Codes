b9fd0db93037ce95bbaecb232fe6a1597ee0d115
SajalDevnath <sajaldevnath96@gmail.com>
2022-06-08 00:24:35 +0530

    8 June

---
+++ practicepattern4.java
@@ -0,0 +1,23 @@
+package com.javacodes;
+
+import java.util.Scanner;
+
+public class practicepattern4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i = 1; i <= n; i++){
+            for(int k = n - 1; k >= i; k--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
